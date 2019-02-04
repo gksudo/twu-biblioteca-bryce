0803a598a5931afa3ecc39de41935c58f4b73cf9
Bryce <gksudo@bando.attlocal.net>
2019-02-03 19:10:20 -0600

    [bryce] [#1.2] added list of library books

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,24 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 
 public class ExampleTest {
 
     @Test
     public void test() {
-        assertEquals(1, 1);
+        assertThat(1, is(1));
+    }
+
+    @Test
+    public void bibliotecaHasBooks() {
+        BibliotecaApp library = new BibliotecaApp();
+
+        assertThat(library.hasBooks(), is(true));
     }
 }
