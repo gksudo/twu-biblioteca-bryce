7a7f4abefc9b7caf4cbbd2b985cfe70355ed1b01
Bryce <gksudo@bando.attlocal.net>
2019-02-03 19:04:34 -0600

    [bryce] [#1.1] added biblioteca welcome message & initial setup

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,15 @@
 package com.twu.biblioteca;
 
+
 public class BibliotecaApp {
+    public BibliotecaApp() {
+        // welcome message
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        BibliotecaApp library = new BibliotecaApp();
     }
 }
